196448fd723800d8fe8ec7db51f01d57cb3ef4bf
user.name <1226438423@qq.com>
2019-12-07 21:01:26 +0800

    Fit add

--- BipedalRobot/Rotate.m
+++
@@ -1,3 +0,0 @@
-function T=Rotate(i)
-global uLINK
-T=[uLINK(i).R,uLINK(i).p;0 0 0 1]
--- BipedalRobot/write.m
+++
@@ -1,9 +0,0 @@
-function write(q)
-global th
-%a=find(q);
-
-for i=1:length(q)
-    
-    th(i)=q(i)*pi/180;
-end
-wholebody;
